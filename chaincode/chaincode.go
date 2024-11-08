66d4dedcd9a46e453eaa9212377903bb19a03bc2
akhileswarv <akhileswarv@ideyalabs.com>
2024-11-08 10:44:02 +0530

    commit

--- chaincode/chaincode.go
+++ chaincode/chaincode.go
@@ -44,7 +44,7 @@ func (r *RealEstate) RegisterUser(ctx contractapi.TransactionContextInterface, u
 		return fmt.Errorf("failed to read user from world state: %v", err)
 	}
 	if userExists != nil {
-		return fmt.Errorf("User Already exist")
+		return fmt.Errorf("User already exists")
 	}
 	user := User{UserId: userId, Name: name, Address: address, Contact: contact}
 	userJson, err := json.Marshal(user)
@@ -59,104 +59,99 @@ func (r *RealEstate) RegisterUser(ctx contractapi.TransactionContextInterface, u
 
 }
 
-func(r *RealEstate) RegisterProperty(ctx contractapi.TransactionContextInterface,propertyId string,title string,location string,size float64,currentOwnerId string,price float64,isListed bool) error{
-	propertyExists,err:=ctx.GetStub().GetState(propertyId)
-	if(err!=nil){
-		return fmt.Errorf("failed to read property from world state: %v",err)
+func (r *RealEstate) RegisterProperty(ctx contractapi.TransactionContextInterface, propertyId string, title string, location string, size float64, currentOwnerId string, price float64, isListed bool) error {
+	propertyExists, err := ctx.GetStub().GetState(propertyId)
+	if err != nil {
+		return fmt.Errorf("failed to read property from world state: %v", err)
 	}
-	if propertyExists!=nil{
+	if propertyExists != nil {
 		return fmt.Errorf("property Already exist")
 	}
-	property:=Property{Id: propertyId,Title: title,Location: location,Size: size,CurrentOwnerId: currentOwnerId,Price: price,IsListed: isListed}
-	propertyJson,err:=json.Marshal(property)
+	property := Property{Id: propertyId, Title: title, Location: location, Size: size, CurrentOwnerId: currentOwnerId, Price: price, IsListed: isListed}
+	propertyJson, err := json.Marshal(property)
 	if err != nil {
 		return fmt.Errorf("failed to convert property struct to json : %v", err)
 	}
-	err=ctx.GetStub().PutState(propertyId,propertyJson)
+	err = ctx.GetStub().PutState(propertyId, propertyJson)
 
 	if err != nil {
 		return fmt.Errorf("failed to put property in world state: %v", err)
 	}
 	return nil
-	
+
 }
 
-
-func (r *RealEstate) TransferPropertyOwnerShip(ctx contractapi.TransactionContextInterface,propertyId string,buyerId string,sellerId string,amount float64,data string) error{
-	propertyBytes,err:=ctx.GetStub().GetState(propertyId)
-	if err!=nil{
-		return fmt.Errorf("failed to read property from world state: %v",err)
+func (r *RealEstate) TransferPropertyOwnerShip(ctx contractapi.TransactionContextInterface, propertyId string, buyerId string, sellerId string, amount float64, data string) error {
+	propertyBytes, err := ctx.GetStub().GetState(propertyId)
+	if err != nil {
+		return fmt.Errorf("failed to read property from world state: %v", err)
 	}
 
-	if(propertyBytes==nil){
+	if propertyBytes == nil {
 		return fmt.Errorf("property not found")
 	}
 
 	var property Property
-	err=json.Unmarshal(propertyBytes,&property)
-	if(err!=nil){
-		return fmt.Errorf("failed to unmarshal property: %v",err)
+	err = json.Unmarshal(propertyBytes, &property)
+	if err != nil {
+		return fmt.Errorf("failed to unmarshal property: %v", err)
 	}
 
-	if !property.IsListed{
+	if !property.IsListed {
 		return fmt.Errorf("property is not listed to sale")
 	}
 
-	if(property.CurrentOwnerId!=sellerId){
+	if property.CurrentOwnerId != sellerId {
 		return fmt.Errorf("seller is the not the current owner of the property")
 	}
 
-	if property.CurrentOwnerId==buyerId{
+	if property.CurrentOwnerId == buyerId {
 		return fmt.Errorf("buyer cannot be the current owner")
 	}
 
-	property.CurrentOwnerId=buyerId
-	property.IsListed=false
+	property.CurrentOwnerId = buyerId
+	property.IsListed = false
 
-	updatedPropertyJson,err:=json.Marshal(property)
-	if(err!=nil){
-		return fmt.Errorf("failed to  marshal updated property: %v",err)
+	updatedPropertyJson, err := json.Marshal(property)
+	if err != nil {
+		return fmt.Errorf("failed to  marshal updated property: %v", err)
 	}
-	err=ctx.GetStub().PutState(propertyId,updatedPropertyJson)
-	if err!=nil{
-		return fmt.Errorf("failed to update property in world state: %v",err)
+	err = ctx.GetStub().PutState(propertyId, updatedPropertyJson)
+	if err != nil {
+		return fmt.Errorf("failed to update property in world state: %v", err)
 	}
-	transaction:=Transaction{Id:fmt.Sprintf("TXN-%s-%s-%s",sellerId,buyerId,propertyId),PropertyId: propertyId,BuyerId: buyerId,SellerId: sellerId,Amount: amount,Date: data,Status: "Complted"}
+	transaction := Transaction{Id: fmt.Sprintf("TXN-%s-%s-%s", sellerId, buyerId, propertyId), PropertyId: propertyId, BuyerId: buyerId, SellerId: sellerId, Amount: amount, Date: data, Status: "Complted"}
 
-	transactionJson,err:=json.Marshal(transaction)
-	if(err!=nil){
-		return fmt.Errorf("failed to marshal transaction: %v",err)
+	transactionJson, err := json.Marshal(transaction)
+	if err != nil {
+		return fmt.Errorf("failed to marshal transaction: %v", err)
 	}
-	err=ctx.GetStub().PutState(transaction.Id,transactionJson)
-	if err!=nil{
-		return fmt.Errorf("failed to save transaction to world state: %v",err)
+	err = ctx.GetStub().PutState(transaction.Id, transactionJson)
+	if err != nil {
+		return fmt.Errorf("failed to save transaction to world state: %v", err)
 	}
 	return nil
 
-
 }
 
-func (r *RealEstate) GetAllUsers(ctx  contractapi.TransactionContextInterface)([]User,error){
+func (r *RealEstate) GetAllDetails(ctx contractapi.TransactionContextInterface) ([]User, error) {
 	var users []User
-	resultIterator,err:=ctx.GetStub().GetStateByRange("","")
-	if err!=nil{
-		return nil,fmt.Errorf("failed to get users: %v",err)
+	resultIterator, err := ctx.GetStub().GetStateByRange("", "")
+	if err != nil {
+		return nil, fmt.Errorf("failed to get users: %v", err)
 	}
 	defer resultIterator.Close()
-	for resultIterator.HasNext(){
-		queryResponse,err:=resultIterator.Next()
-		if err!=nil{
-			return nil,fmt.Errorf("Failed to iterate over users: %v",err)
+	for resultIterator.HasNext() {
+		queryResponse, err := resultIterator.Next()
+		if err != nil {
+			return nil, fmt.Errorf("failed to iterate over users: %v", err)
 		}
 		var user User
-		err=json.Unmarshal(queryResponse.Value,&user)
-        if(err!=nil){
-			return nil,fmt.Errorf("failed to unmarshal user data: %v",err)
+		err = json.Unmarshal(queryResponse.Value, &user)
+		if err != nil {
+			return nil, fmt.Errorf("failed to unmarshal user data: %v", err)
 		}
-		users=append(users, user)
+		users = append(users, user)
 	}
-	return users,nil
+	return users, nil
 }
-
-
-
